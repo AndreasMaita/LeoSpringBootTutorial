3392cb2372f42d7872679ad5ffc1aab5b6490b92
Maita, Andreas <Andreas.Maita@adesso.de>
2022-11-15 12:51:05 +0100

    habe die Aufgaben hinzugefügt

---
+++ src/main/java/com/example/leo/CarController.java
@@ -0,0 +1,17 @@
+package com.example.leo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CarController {
+    // Hi Leo!
+    // Aufgabe 1: Bitte schreibe eine GET Methode, die mir alle Automarken schickt die wir kennen.
+
+    // Aufgabe 2: Schreibe eine POST Methode, die eine Automarke( in Form eines Strings) nimmt,
+    // und sie speichert.
+
+    // Aufgabe 3: Schreibe eine PUT Methode, um Mercedes in Mercedes-Benz umzubennen.
+}
